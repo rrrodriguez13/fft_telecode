50ab2873b06a9f524273b3d7f52f8fff36d6754e
rrrodriguez13 <rrrodriguez13@berkeley.edu>
2024-06-25 16:48:16 -0700

    good changes?

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 import time
-from udp_script import send, receive
+from udp_script import send, receive, MAX_UDP_PACKET_SIZE
+
+def capture_data(size):
+    # send a bunch of zeros for now, replace with actual data later
+    return [0] * size
 
 def sender_main():
-    UDP = send('192.168.0.234', 2001, '192.168.0.123', 2002, 900e6)
+    UDP = send('192.168.0.234', 2001)
     UDP.eth0()
-    UDP.eth1()
     print('Everything initialized...')
     try:
         print('Starting loop...')
         while True:
-            UDP.udp0()
+            data = capture_data(3*MAX_UDP_PACKET_SIZE)
+            UDP.send_data(data)
             time.sleep(1)
+            print("sent data")
     except KeyboardInterrupt:
         UDP.stop()
         print('UDP Stopped...')
@@ -32,5 +37,5 @@ def receiver_main():
 
 if __name__ == "__main__":
     # Uncomment the appropriate function depending on whether you want to send or receive data
-    # sender_main()
-    receiver_main()
+    sender_main()
+    #receiver_main()
--- udp_script.py
+++ udp_script.py
@@ -7,43 +7,23 @@ import time
 MAX_UDP_PACKET_SIZE = 65507  # Maximum safe UDP packet size
 
 class send:
-    def __init__(self, HOST, PORT, HOST1, PORT1, size):
+    def __init__(self, HOST, PORT):
         self.HOST = HOST
         self.PORT = PORT
-        self.HOST1 = HOST1
-        self.PORT1 = PORT1
-        self.size = int(size)
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     def stop(self):
         self.s.close()
-        self.s1.close()
-        self.thrd.join()
     
     def eth0(self):
         self.s.connect((self.HOST, self.PORT))
         print('eth0 starting...')
         
-    def eth1(self):
-        self.s1.connect((self.HOST1, self.PORT1))
-        print('eth1 starting...')
     
-    def udp0(self):
-        self.thrd = threading.Thread(target=self.udp1)
-        self.thrd.start()
-        self.send_data(self.s, self.HOST, self.PORT)
-        print('eth0 sent data...')
-    
-    def udp1(self):
-        self.send_data(self.s1, self.HOST1, self.PORT1)
-        print('eth1 sent data...')
-    
-    def send_data(self, sock, host, port):
-        data = bytearray(self.size)
-        chunks = [data[i:i+MAX_UDP_PACKET_SIZE] for i in range(0, len(data), MAX_UDP_PACKET_SIZE)]
+    def send_data(self, data):
+        chunks = data.reshape(-1, MAX_UDP_PACKET_SIZE)
         for chunk in chunks:
-            sock.sendto(chunk, (host, port))
+            self.s.sendto(chunk, (self.HOST, self.PORT))
 
 class receive:
     def __init__(self, HOST, PORT):
