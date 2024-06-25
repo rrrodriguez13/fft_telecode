ca3c7e5b31f28c6372aef43547504e3c38b41c67
rrrodriguez13 <rrrodriguez13@ugastro.berkeley.edu>
2024-06-25 14:44:42 -0700

    changed maximum safe UDP packet size in udp_script.py

--- udp_script.py
+++ udp_script.py
@@ -4,13 +4,15 @@ import sys
 import threading
 import time
 
+MAX_UDP_PACKET_SIZE = 65507  # Maximum safe UDP packet size
+
 class send:
     def __init__(self, HOST, PORT, HOST1, PORT1, size):
         self.HOST = HOST
         self.PORT = PORT
         self.HOST1 = HOST1
         self.PORT1 = PORT1
-        self.size = bytearray(int(size))
+        self.size = int(size)
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
@@ -30,14 +32,18 @@ class send:
     def udp0(self):
         self.thrd = threading.Thread(target=self.udp1)
         self.thrd.start()
-        x = self.size
-        self.s.sendto(x, (self.HOST, self.PORT))
+        self.send_data(self.s, self.HOST, self.PORT)
         print('eth0 sent data...')
     
     def udp1(self):
-        x = self.size
-        self.s1.sendto(x, (self.HOST1, self.PORT1))
+        self.send_data(self.s1, self.HOST1, self.PORT1)
         print('eth1 sent data...')
+    
+    def send_data(self, sock, host, port):
+        data = bytearray(self.size)
+        chunks = [data[i:i+MAX_UDP_PACKET_SIZE] for i in range(0, len(data), MAX_UDP_PACKET_SIZE)]
+        for chunk in chunks:
+            sock.sendto(chunk, (host, port))
 
 class receive:
     def __init__(self, HOST, PORT):
@@ -50,7 +56,7 @@ class receive:
         print('Set up connection...')
     
     def set_up(self):
-        data, addr = self.s.recvfrom(int(900e6 + 57))
+        data, addr = self.s.recvfrom(MAX_UDP_PACKET_SIZE)
         print('received data:', data)
     
     def stop(self):
