27b0d076007043452903e25b72caaec8dde17cc9
rrrodriguez13 <rrrodriguez13@ugastro.berkeley.edu>
2024-06-27 15:04:20 -0700

    fixing

--- testing/udp_script.py
+++ testing/udp_script.py
@@ -27,28 +27,6 @@ class send:
             self.s.sendto(chunks, (self.HOST, self.PORT))
             print(f'Sent chunk {i+1}/{len(chunks)} of size {len(chunk)}')
 
-# class send:
-#     def __init__(self, HOST, PORT):
-#         self.HOST = HOST
-#         self.PORT = PORT
-#         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-#     def stop(self):
-#         self.s.close()
-    
-#     def eth0(self):
-#         self.s.connect((self.HOST, self.PORT))
-#         print('eth0 starting...')
-        
-#     def send_data(self, data):
-#         # Ensure data is in bytes format
-#         data = bytearray(data)
-#         # Split data into chunks
-#         chunks = [data[i:i + MAX_UDP_PACKET_SIZE] for i in range(0, len(data), MAX_UDP_PACKET_SIZE)]
-#         for i, chunk in enumerate(chunks):
-#             self.s.sendto(chunk, (self.HOST, self.PORT))
-#             print(f'Sent chunk {i+1}/{len(chunks)} of size {len(chunk)}')
-
 class receive:
     def __init__(self, HOST, PORT):
         self.HOST = HOST
