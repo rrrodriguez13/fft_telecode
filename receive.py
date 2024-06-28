e2dfc9d53e28707e1651956f8505967292be0c23
rrrodriguez13 <rrrodriguez13@ugastro.berkeley.edu>
2024-06-28 11:33:09 -0700

    reverting bad changes

--- receive.py
+++ receive.py
@@ -1,24 +1,20 @@
-from functions import receive, MAX_UDP_PACKET_SIZE, set_up_plot, plotter
-import numpy as np
+from functions import receive, MAX_UDP_PACKET_SIZE
 
 LAPTOP_IP = "192.168.0.234"
 RPI_IP = "192.168.0.235"
 PORT = 6371
 
+def capture_data(size):
+    # send a bunch of zeros for now, replace with actual data later
+    return [0] * size
+
 def receiver_main():
     UDP = receive(LAPTOP_IP, PORT)
     UDP.eth0()
     print('Everything Initialized...')
-
-    fig, line = set_up_plot()
-
     try:
         while True:
-            data = UDP.set_up()
-            if data:
-                spectrum = np.frombuffer(data, dtype=np.float64)
-                plotter(spectrum, fig, line)
-
+            UDP.set_up()
     except KeyboardInterrupt:
         UDP.stop()
         print('UDP Stopped...')
@@ -28,4 +24,3 @@ def receiver_main():
 if __name__ == "__main__":
     # receiving script (not sending)
     receiver_main()
-
