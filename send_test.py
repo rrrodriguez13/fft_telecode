152e6abd36b9708c9540d6d4b31d52b02c8c4ef4
rrrodriguez13 <rrrodriguez13@ugastro.berkeley.edu>
2024-08-19 11:46:26 -0700

    trying without capture thread since it seems broken maybe

--- send_test.py
+++ send_test.py
@@ -25,7 +25,7 @@ if not os.path.exists(folder):
     os.makedirs(folder)
 
 # sets up SDR
-sdr = ugradio.sdr.SDR(sample_rate=2.2e6, center_freq=145.2e6, direct=False, gain=10)
+sdr = ugradio.sdr.SDR(sample_rate=2.2e6, center_freq=145.2e6, direct=False, gain=20)
 
 # sets up network connection
 UDP = send(LAPTOP_IP, PORT)
@@ -33,31 +33,14 @@ UDP = send(LAPTOP_IP, PORT)
 data_queue = queue.Queue(maxsize=0)  # infinite size queue to prevent data loss
 stop_event = threading.Event()
 
-def data_capture():
-    try:
-        while not stop_event.is_set():
-            list = np.arange(1, num_samples, dtype=int) # list of integers to attach to data
-            data = sdr.capture_data(num_samples) # data
-            array = np.column_stack((list, data)) # array with integers added in first column
-            print(f"Captured data: {array.shape}") # prints shape of data captured
-            data_queue.put(array)
-    except KeyboardInterrupt:
-        stop_event.set()
-    finally:
-        data_queue.put(None)  # signal sender threads to exit
-        for thread in sender_threads:
-            thread.join()
-        print("Main thread done.")
-
 def data_sender():
     try:
         cnt = 0
         while not stop_event.is_set() or not data_queue.empty():
-            data_array = data_queue.get()
-            if data_array is None:
+            d = data_queue.get()
+            if d is None:
                 break
-            print(f"Sending data: {data_array.shape}")  # prints shape of data being sent
-            UDP.send_data(data_array)
+            UDP.send_data(d)
             cnt += 1
             print(f"Sent Data! cnt={cnt}")
     except Exception as e:
@@ -66,23 +49,24 @@ def data_sender():
         UDP.stop()
         print("Data transfer stopped.")
 
-
 # increases the number of sender threads to handle data faster
 num_sender_threads = 3  # can adjust based on what system can handle
 sender_threads = [threading.Thread(target=data_sender) for _ in range(num_sender_threads)]
-capture_thread = threading.Thread(target=data_capture)
 
-# starts threads
-for thread in capture_thread:
-    capture_thread.start()
 for thread in sender_threads:
     thread.start()
 
-
-# joins threads to wait for completion
-for thread in capture_thread:
-    capture_thread.join()
-for thread in sender_threads:
-    thread.join()
-
-print("All threads have completed.")
+try:
+    while not stop_event.is_set():
+        list = np.arange(1, num_samples, dtype=int) # list of integers to attach to data
+        data = sdr.capture_data(num_samples) # data
+        array = np.column_stack((list, data)) # array with integers added in first column
+        print(f"Captured data: {array.shape}") # prints shape of data captured
+        data_queue.put(array)
+except KeyboardInterrupt:
+    stop_event.set()
+finally:
+    data_queue.put(None)  # signal sender threads to exit
+    for thread in sender_threads:
+        thread.join()
+    print("Main thread done.")
