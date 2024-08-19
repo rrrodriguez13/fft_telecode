142e5786e2f34d9c5453d277c40e0f9369e16aed
rrrodriguez13 <rrrodriguez13@ugastro.berkeley.edu>
2024-08-19 12:39:34 -0700

    changes

--- send_test.py
+++ send_test.py
@@ -36,8 +36,10 @@ stop_event = threading.Event()
 def data_capture():
     try:
         while not stop_event.is_set():
-            list = np.arange(0, num_samples, dtype=int) # list of integers to attach to data
+            lst = np.arange(0, num_samples, dtype=int) # list of integers to attach to data
             data = sdr.capture_data(num_samples) # data
+            print(lst.shape)
+            print(data.shape)
             #data = d.reshape(2*num_samples, 1) # reshaping data for array with list of integers
             array = np.column_stack((list, data)) # array defined as 2 columns for integers and data
             print(f"Captured data: {array.shape}") # prints shape of data captured
